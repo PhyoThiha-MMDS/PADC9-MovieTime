8c50efe473f0cde36feff7f81267e239d9faf1f5
PhyoThiha-MMDS <phyo.thiha@mm-digital-solutions.com>
2019-06-12 01:15:09 +0630

    Initial Project Setup

---
+++ MovieTime/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MovieTime
+//
+//  Created by Phyo Thiha on 12/06/2019.
+//  Copyright © 2019 padc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
